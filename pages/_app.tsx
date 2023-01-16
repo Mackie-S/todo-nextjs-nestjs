0eb0a17edf23a0bedb6b66548f568ef7bd994f2e
Mackie <masaki.shimadu@gmail.com>
2023-01-16 11:33:49 +0900

    feat: todo init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,75 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import axios from 'axios'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  axios.defaults.withCredentials = true
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/csrf`
+      )
+      axios.defaults.headers.common['csrf-token'] = data.csrfToken
+    }
+    getCsrfToken()
+  }, [])
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'dark',
+          fontFamily: 'Verdana, sans-serif',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
 }
+
+// import { AppProps } from 'next/app'
+// import Head from 'next/head'
+
+// export default function App(props: AppProps) {
+//   const { Component, pageProps } = props
+
+//   return (
+//     <>
+//       <Head>
+//         <title>Page title</title>
+//         <meta
+//           name="viewport"
+//           content="minimum-scale=1, initial-scale=1, width=device-width"
+//         />
+//       </Head>
+
+//       <MantineProvider
+//         withGlobalStyles
+//         withNormalizeCSS
+//         theme={{
+//           /** Put your mantine theme override here */
+//           colorScheme: 'light',
+//         }}
+//       >
+//         <Component {...pageProps} />
+//       </MantineProvider>
+//     </>
+//   )
+// }
